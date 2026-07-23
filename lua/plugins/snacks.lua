ca0313b848c6777c68bf44f128aae2031de6ffb0
PhilippeQU <philippe.qu@outlook.com>
2026-07-23 10:13:52 +0800

    feat: 将leader+n提示历史右侧面板默认折叠换行

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,6 +6,19 @@ return {
         db = {
           sqlite3_path = vim.fn.stdpath("data") .. "/sqlite3.dll",
         },
+        sources = {
+          notifications = {
+            win = {
+              preview = {
+                wo = {
+                  wrap = true,
+                  linebreak = true,
+                  breakindent = true,
+                },
+              },
+            },
+          },
+        },
       },
     },
   },
